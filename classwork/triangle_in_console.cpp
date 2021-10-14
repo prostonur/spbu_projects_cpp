a16ba401455ca36e5ea298af4e7f4ce2ed0d805d
almostinf <almostinf@mail.ru>
2021-10-14 15:27:57 +0300

    recast zvezdi in triangle_in_console.cpp

---
+++ classwork/triangle_in_console.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+int main() {
+    size_t n;
+    std::cout << "enter n: ";
+    std::cin >> n;
+    int tabs = 1;
+    for (size_t i = 1; i <= 2 * n - 1; i += 2) {
+        int temp_tabs = 0;
+        while (temp_tabs != n - tabs)
+        {
+            std::cout << ' ';
+            temp_tabs++;
+        }
+        size_t zvezdi = i;
+        while (zvezdi)
+        {
+            std::cout << '*';
+            zvezdi--;
+        }
+        std::cout << '\n';
+        tabs++;
+    }
+}
+
