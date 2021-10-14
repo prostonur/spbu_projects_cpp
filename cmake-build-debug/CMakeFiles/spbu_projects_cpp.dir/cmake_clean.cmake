0cceefc2d6649650db2b9524e451af93dadb3008
almostinf <almostinf@mail.ru>
2021-10-14 14:50:59 +0300

    creating new repository

---
+++ cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spbu_projects_cpp.dir/main.cpp.obj"
+  "libspbu_projects_cpp.dll.a"
+  "spbu_projects_cpp.exe"
+  "spbu_projects_cpp.exe.manifest"
+  "spbu_projects_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spbu_projects_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
