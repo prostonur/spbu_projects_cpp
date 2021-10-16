91dfc0126517f4f279ef1c80a3461b39ab76b11b
almostinf <almostinf@mail.ru>
2021-10-16 19:43:23 +0300

    added condition in unsigned_char_values.cpp

--- cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/cmake_clean.cmake
@@ -1,5 +1,6 @@
 file(REMOVE_RECURSE
   "CMakeFiles/spbu_projects_cpp.dir/homework/hollow_rhombus.cpp.obj"
+  "CMakeFiles/spbu_projects_cpp.dir/homework/unsigned_char_values.cpp.obj"
   "libspbu_projects_cpp.dll.a"
   "spbu_projects_cpp.exe"
   "spbu_projects_cpp.exe.manifest"
