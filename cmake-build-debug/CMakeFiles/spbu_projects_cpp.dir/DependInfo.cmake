b935a34b69687675de1cb40e1dc922db7a9ed139
almostinf <almostinf@mail.ru>
2021-10-16 20:31:19 +0300

    added condition in powers_row.cpp

--- cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/DependInfo.cmake
@@ -8,6 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/My Projects C++/spbu projects cpp/homework/powers_row.cpp" "C:/My Projects C++/spbu projects cpp/cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/homework/powers_row.cpp.obj"
   "C:/My Projects C++/spbu projects cpp/homework/ring_with_radiusis.cpp" "C:/My Projects C++/spbu projects cpp/cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/homework/ring_with_radiusis.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
--- cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/cmake_clean.cmake
@@ -1,4 +1,5 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/spbu_projects_cpp.dir/homework/powers_row.cpp.obj"
   "CMakeFiles/spbu_projects_cpp.dir/homework/ring_with_radiusis.cpp.obj"
   "libspbu_projects_cpp.dll.a"
   "spbu_projects_cpp.exe"
