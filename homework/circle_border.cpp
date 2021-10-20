7f534d6af864d5e662e17221400f5698fc6e2869
almostinf <almostinf@mail.ru>
2021-10-20 23:12:12 +0300

    added solution in circle_border.cpp

--- homework/circle_border.cpp
+++ homework/circle_border.cpp
@@ -2,3 +2,56 @@
 //Посчитать число точек с целочисленными координатами, лежащих внутри окружности,
 //в том числе и находящихся на границе окружности.
 
+#include <iostream>
+#include <cmath>
+#include <set>
+
+
+int main() {
+    int r;
+    std::set<std::pair<int, int>> st;
+    std::cout << "enter r: ";
+    std::cin >> r;
+    int count = 0;
+    for (int x = -r; x <= r; ++x)
+        for (int y = -r; y <= r; ++y)
+            if (pow(x,2) + pow(y, 2) <= pow(r, 2))
+                st.emplace(std::make_pair(x,y));
+    std::cout << "number of dots: " << st.size() << '\n';
+}
+
+// another solution
+//int main() {
+//    int r;
+//    std::cout << "enter r: ";
+//    std::cin >> r;
+//    bool arr[2 * r + 1][2 * r + 1];
+//    for (int x = -r; x <= r; ++x) {
+//        for (int y = -r; y <= r; ++y)
+//            arr[x][y] = false;
+//    }
+//    for (int x = -r; x <= r; ++x) {
+//        for (int y = -r; y <= r; ++y)
+//            std::cout << arr[x][y] << ' ';
+//        std::cout << '\n';
+//    }
+//
+//
+//    int count = 0;
+//    for (int x = -r; x <= r; ++x)
+//        for (int y = -r; y <= r; ++y)
+//            if (pow(x,2) + pow(y, 2) <= pow(r, 2)) {
+//                if (!arr[x][y]) {
+//                    count++;
+//                    arr[x][y] = true;
+//                }
+//            }
+//    std::cout << "----------------------------------------\n";
+//    for (int x = -r; x <= r; ++x) {
+//        for (int y = -r; y <= r; ++y)
+//            std::cout << arr[x][y] << ' ';
+//        std::cout << '\n';
+//    }
+//    std::cout << "number of dots: " << count << '\n';
+//}
+
