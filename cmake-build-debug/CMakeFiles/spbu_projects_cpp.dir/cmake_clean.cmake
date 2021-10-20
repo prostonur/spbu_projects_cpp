eae27094eac9af9ee1b2b0a911a458ad43686dbe
almostinf <almostinf@mail.ru>
2021-10-20 22:01:24 +0300

    added solution in numbers_or_words.cpp

--- cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/cmake_clean.cmake
@@ -1,6 +1,5 @@
 file(REMOVE_RECURSE
   "CMakeFiles/spbu_projects_cpp.dir/homework/numbers_or_words.cpp.obj"
-  "CMakeFiles/spbu_projects_cpp.dir/homework/powers_row.cpp.obj"
   "libspbu_projects_cpp.dll.a"
   "spbu_projects_cpp.exe"
   "spbu_projects_cpp.exe.manifest"
--- homework/numbers_or_words.cpp
+++ homework/numbers_or_words.cpp
@@ -5,3 +5,18 @@
 //если число делится и на 3 и на 5, вывести слово FooBar;
 //иначе вывести само число
 
+#include <iostream>
+
+
+int main() {
+    for (int i = 1; i <= 100; ++i) {
+        if (i % 3 == 0 and i % 5 == 0)
+            std::cout << "FooBar\n";
+        else if (i % 3 == 0)
+            std::cout << "Foo\n";
+        else if (i % 5 == 0)
+            std::cout << "Bar\n";
+        else
+            std::cout << i << '\n';
+    }
+}
