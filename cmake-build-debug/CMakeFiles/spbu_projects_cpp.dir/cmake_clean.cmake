08432b79b7f27cbfe49e022787db4fd1e396c9e9
almostinf <almostinf@mail.ru>
2021-10-21 22:25:01 +0300

    added solution in graphic_in_console.cpp

--- cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/cmake_clean.cmake
@@ -1,5 +1,4 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/spbu_projects_cpp.dir/homework/circle_border.cpp.obj"
   "CMakeFiles/spbu_projects_cpp.dir/homework/graphic_in_console.cpp.obj"
   "libspbu_projects_cpp.dll.a"
   "spbu_projects_cpp.exe"
--- homework/graphic_in_console.cpp
+++ homework/graphic_in_console.cpp
@@ -4,3 +4,25 @@
 //а последний (width) символ в последней (height) строке соответвует точке (1.5, -1.5).
 //Если координаты точки удовлетворяют неравенству, то выводить символ *, иначе символ .
 
+#include <iostream>
+#include <cmath>
+
+
+bool function(int x, int y) {
+    return pow((pow(static_cast<double>(x) / 10, 2) + pow(static_cast<double>(y) / 10, 2) - 1), 3)
+    - pow(static_cast<double>(x) / 10, 2) * pow(static_cast<double>(y) / 10, 3) <= 0;
+}
+
+
+int main() {
+    for (int x = -15; x <= 15; ++x) {
+         for (int y = -15; y <= 15; ++y) {
+             if (function(x, y))
+                 std::cout << "*";
+             else
+                 std::cout << " ";
+             if (y == 15)
+                 std::cout << '\n';
+         }
+    }
+}
