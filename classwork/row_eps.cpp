5d6c423c9295a781d18e1bbad4cac91c6f470126
almostinf <almostinf@mail.ru>
2021-10-28 18:24:17 +0300

    added condition and fix some troubles in row_eps.cpp

--- classwork/row_eps.cpp
+++ classwork/row_eps.cpp
@@ -1,10 +1,16 @@
+//Написать программу, которая вычисляет число pi с заданной пользователем точностью.
+//Под точностью понимается абсолютная величина очередного слагаемого.
+//Для вычисления значения числа pi воспользоваться формулой: pi = 4 * (1 - 1/3 + 1/5 - 1/7 + 1/9 - ...)
+//Определить какое количество дробей необходимо было добавить к сумме, чтобы обеспечить заданную точность.
+//Запомнить кол-во, например в переменной N.
+
 #include <iostream>
-#include <cmath>
 
 int main() {
     double eps;
     std::cout << "enter eps: ";
     std::cin >> eps;
+    int count_iteration = 1;
     double s = 1;
     int count = 1;
     double sign = 1.0;
@@ -13,13 +19,14 @@ int main() {
     double sign_2 = 1.0;
     while (mult * sign <= eps) {
         sign_2 *= -1.0;
-        mult = sign_2 * (static_cast<double>(1) / i);
+        mult = sign_2 * (1. / i);
         s += mult;
         i += 2;
+        count_iteration++;
         if (mult < 0.0)
             sign = -1.0;
         else
             sign = 1.0;
     }
-    std::cout << s * 4 << '\n';
+    std::cout << s * 4 << ' ' << count_iteration - 1 << '\n';
 }
