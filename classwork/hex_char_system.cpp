8320e5287c69ff500b7050ab6e8baec676056190
almostinf <almostinf@mail.ru>
2021-10-30 18:22:43 +0300

    fix solution in hex_char_system.cpp

---
+++ classwork/hex_char_system.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+int main() {
+    int num;
+    int n = 10;
+    auto* nums = new char[n];
+    for (int i = 0; i < n; ++i)
+        nums[i] = 0;
+    std::cout << "enter num: ";
+    std::cin >> num;
+    int deg = 1;
+    int temp_num = num;
+    while ((temp_num /= 16) != 0)
+        deg *= 16;
+    int count = 0;
+    while (deg) {
+        if (num / deg <= 9) {
+            nums[count] = static_cast<char>(48 + num / deg);
+            count++;
+        }
+        else {
+            nums[count] = static_cast<char>(55 + num / deg);
+            count++;
+        }
+        num %= deg;
+        deg /= 16;
+    }
+    nums[count] = 0;
+    std::cout << nums << '\n';
+    delete[] nums;
+}
+
