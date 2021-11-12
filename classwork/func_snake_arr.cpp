26dd447d834c392606146ad07878c63f793bf557
almostinf <almostinf@mail.ru>
2021-11-12 14:48:17 +0300

    added solution in func_snake_arr.cpp

---
+++ classwork/func_snake_arr.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+void snakeArray(int** arr, int n) {
+    int num = 1;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (i % 2 != 0 and j == n) {
+                arr[i][j] = num;
+                num += n;
+            } else if (i % 2 != 0) {
+                arr[i][j] = num;
+                num++;
+            } else if (i % 2 == 0 and j == n) {
+                arr[i][j] = num;
+                num += n;
+            } else if (i % 2 == 0) {
+                arr[i][j] = num;
+                num--;
+            }
+        }
+    }
+}
+int main() {
+    int n;
+    std::cout << "enter n: ";
+    std::cin >> n;
+    int** arr = new int*[n];
+    for (int i = 1; i <= n; ++i)
+        arr[i] = new int[n];
+    snakeArray(arr, n);
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j)
+            std::cout << arr[i][j] << ' ';
+        std::cout << '\n';
+    }
+    for (int i = 1; i <= n; ++i)
+        delete[] arr[i];
+    delete[] arr;
+}
+
--- cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/My Projects C++/spbu projects cpp/classwork/fiil_random_matrix.cpp" "C:/My Projects C++/spbu projects cpp/cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/classwork/fiil_random_matrix.cpp.obj"
+  "C:/My Projects C++/spbu projects cpp/classwork/func_snake_arr.cpp" "C:/My Projects C++/spbu projects cpp/cmake-build-debug/CMakeFiles/spbu_projects_cpp.dir/classwork/func_snake_arr.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
